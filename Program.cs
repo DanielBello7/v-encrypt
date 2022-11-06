8632d962bd0703400e85496dac3709f30c17d33c
Daniel Bello <gokebello@gmail.com>
2022-11-06 16:52:40 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            // get all the parameters from the cli
+            string type = args[0];
+            string phrase = args[1];
+            string keyword = args[2];
+
+            // get the key by generating it 
+            string key = generateKey(phrase, keyword);
+
+            // perform either encryption or decryption
+            if (type == "encrypt") Console.WriteLine(encrypt(phrase, key));
+            if (type == "decrypt") Console.WriteLine(decrypt(phrase, key));
+        }
+        catch (Exception error)
+        {
+            // catch any error that would occur
+            Console.WriteLine("Error occured: Check the arguments or " + error.Message);
+        }
+    }
+
+    static string generateKey(string phrase, string keyword)
+    {
+        // check if the length of the keyword matches the pass phrase and return if true
+        if (keyword.Length == phrase.Length) return keyword;
+
+        string key = keyword;
+
+        int watch = phrase.Length - keyword.Length;
+
+        // keep repeating the process of adding the next character to the new key
+        for (int i = 0; i < watch; i++) key += (key[i]);
+
+        return key;
+    }
+
+    static string encrypt(string phrase, string key)
+    {
+        string cipher_text = "";
+
+        for (int i = 0; i < phrase.Length; i++)
+        {
+            // using the formula Ei = (Pi + Ki) mod 26 to get the position of the character in the ASCII chart
+            int position = (phrase[i] + key[i]) % 26;
+
+            // convert it to character
+            position += 'A';
+
+            cipher_text += (char)(position);
+        }
+
+        return cipher_text;
+    }
+
+    static string decrypt(string phrase, string key)
+    {
+        string response = "";
+
+        for (int i = 0; i < phrase.Length && i < key.Length; i++)
+        {
+            int position = (phrase[i] - key[i] + 26) % 26;
+
+            position += 'A';
+
+            response += (char)(position);
+        }
+
+        return response;
+
+    }
+}
+
+// note that both keyword and phrase must be in capital for this program to work. Haven't figured it out yet
---
+++ README.md
@@ -0,0 +1,39 @@
+﻿# V-ENCRYPT
+
+This program converts a phrase into a cipher text based off of a key given
+
+The program is written in C# and could be run using both the .dll file or compiled from the program.cs file
+
+## ARGULEMTS
+
+The program takes in 3 arguments
+    - process type
+    - phrase
+    - keyword
+
+    ### PROCESS-TYPE
+    The process type can be either "encrypt" or "decrypt", anything other than that would raise an error
+
+    ### PHRASE
+    The phrase is the phrase to either be encrypted or decrypted, and should be all in CAPS
+
+    ### KEYWORD
+    The keyword is what would be used to make a key that would be used to encrypt or decrypt the phrase
+    
+
+## RUN EXAMPLE
+
+DOTNET can be used to run the Program.cs file as so
+- dotnet run encrypt JOE AYU
+
+    i. the process type being "encrypt"
+    ii. the phrase being "JOE"
+    iii. the keyword being "AYU"
+
+DOTNET can also be used to run the .dll file as so
+- dotnet VENCRYPT.dll encrypt JOE AYU
+
+
+The program could be compiled into a .exe file and run directly as so
+- csc Program.cs
+- Program.exe
